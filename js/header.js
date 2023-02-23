b6e70d12c5848d79fb54b9ef562c4c5c333aeb42
arthurmkrtchian <mkrtchian.arthur999@gmail.com>
2023-02-23 23:21:35 +0200

    set region-coat margin-top value using js

--- js/header.js
+++ js/header.js
@@ -2,4 +2,5 @@ window.onload = function () {
   var header = document.getElementById('header')
   var number = parseInt(window.getComputedStyle(header).height)
   document.getElementById('header').style.marginBottom = number + 'px';
+  document.getElementById('region-coat').style.marginTop = number + 'px';
 }
