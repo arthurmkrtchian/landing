0c7e8662fd83f8fa54d0d1f84403a860f572798a
arthurmkrtchian <mkrtchian.arthur999@gmail.com>
2023-02-23 23:15:39 +0200

    big fix pack

--- js/header.js
+++ js/header.js
@@ -1,5 +1,5 @@
-window.onload = function() {
-    var header = document.getElementById('header')
-    var number = parseInt(window.getComputedStyle(header).height)
-    document.getElementById('header').style.marginBottom = number + 'px';
-  }
+window.onload = function () {
+  var header = document.getElementById('header')
+  var number = parseInt(window.getComputedStyle(header).height)
+  document.getElementById('header').style.marginBottom = number + 'px';
+}
