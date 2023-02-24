55cf8adb83f201bfdd9f2224d8b63c98f9252c41
arthurmkrtchian <mkrtchian.arthur999@gmail.com>
2023-02-24 14:20:45 +0200

    resolved many minor bugs

--- js/header.js
+++ js/header.js
@@ -2,7 +2,7 @@
   window.onload = function () {
     var header = document.getElementById('header')
     var number = parseInt(window.getComputedStyle(header).height)
-    document.getElementById('header').style.marginBottom = number + 'px';
+    document.getElementById('header').style.marginBottom = (number -40)+ 'px';
     document.getElementById('region-coat').style.marginTop = number + 'px';
   }
 })();
