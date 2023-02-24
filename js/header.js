0fd1207a8cf54de25f3e0e1560236e3702fbd92c
arthurmkrtchian <mkrtchian.arthur999@gmail.com>
2023-02-24 12:11:03 +0200

    added Our digital products and added minor fixes

--- js/header.js
+++ js/header.js
@@ -1,6 +1,8 @@
-window.onload = function () {
-  var header = document.getElementById('header')
-  var number = parseInt(window.getComputedStyle(header).height)
-  document.getElementById('header').style.marginBottom = number + 'px';
-  document.getElementById('region-coat').style.marginTop = number + 'px';
-}
+(function(){
+  window.onload = function () {
+    var header = document.getElementById('header')
+    var number = parseInt(window.getComputedStyle(header).height)
+    document.getElementById('header').style.marginBottom = number + 'px';
+    document.getElementById('region-coat').style.marginTop = number + 'px';
+  }
+})();
